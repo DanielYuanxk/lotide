802438ff52d5efaa9e2829537b654a13a0817242
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-10 18:23:43 -0500

    array comparison

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  let isGood = true;
+  if (array1.length != array2.length) {
+    isGood = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isGood = false;
+    }
+  }
+  return isGood;
+};
+
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true);
--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,7 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-let head = function (array) {
+let head = function(array) {
   let first = array[0];
   return first;
 };
