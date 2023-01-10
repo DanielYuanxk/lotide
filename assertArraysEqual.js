34237ccf5f2a38e7eaaeb3980e60ebe3e4e81cce
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-10 18:32:47 -0500

    equal arrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = (array1, array2) => {
+  let isGood = true;
+  if (array1.length != array2.length) {
+    isGood = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isGood = false;
+    }
+  }
+  if (isGood) {
+    console.log("Your Arrays are Equal!");
+  } else {
+    console.log("Your Arrays are not the same.");
+  }
+};
+
+eqArrays([1, 3, 5], [1, 3, 4]);
+eqArrays([1, 3, 5], [1, 3, 5]);
