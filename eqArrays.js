fb2a8b46e3bf559f2b04299bfe850d4dc63d9191
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-16 07:50:02 -0500

    eqArrays refactor module exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (array1, array2) => {
   let isGood = true;
   if (array1.length != array2.length) {
@@ -19,4 +11,4 @@ const eqArrays = (array1, array2) => {
   return isGood;
 };
 
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true);
+module.exports = eqArrays;
