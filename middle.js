8346aea845aea9ad01a4fe5e29512a2cedd9628f
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-16 08:01:28 -0500

    middle refactored module exports

--- middle.js
+++ middle.js
@@ -1,23 +1,5 @@
-const eqArrays = (array1, array2) => {
-  let isGood = true;
-  if (array1.length != array2.length) {
-    isGood = false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      isGood = false;
-    }
-  }
-  return isGood;
-};
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2) === true) {
-    console.log("👍👍👍 Your Arrays are Equal!");
-  } else {
-    console.log("🫵🫵🫵 Your Arrays are not the same.");
-  }
-};
-
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 const middle = (array) => {
   let newArray = [];
   if (array.length <= 1) {
@@ -30,7 +12,4 @@ const middle = (array) => {
   }
   return newArray;
 };
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4, 5]);
+module.exports = middle;
