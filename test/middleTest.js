ff443b5b40511c553aa68080363bf2cfd0718ac0
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-16 09:13:54 -0500

    middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,11 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4, 5]);
+describe("#middle", () => {
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("should return [4,5] for [3,4,5,6]", () => {
+    assert.deepEqual(middle([3, 4, 5, 6]), [4, 5]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,11 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [4] for [2,4]", () => {
+    assert.deepEqual(tail([2, 4]), [4]);
+  });
+  it("returns [5,6,7] for [4,5,6,7]", () => {
+    assert.deepEqual(tail([4, 5, 6, 7]), [5, 6, 7]);
+  });
+});
