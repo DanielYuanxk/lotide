9e30acc52e9d3dbc7280a2417d4d5d7938233a05
DanielYuanxk <dyxk1234@hotmail.com>
2023-01-16 09:58:55 -0500

    adding index and puting function into objects

---
+++ index.js
@@ -0,0 +1,15 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArrayEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArrayEqual: assertArrayEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+};
